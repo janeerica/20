dbe8dcd7cfaeb0032e1c52b41dea89ffcb116758
janeerica <76727311+janeerica@users.noreply.github.com>
2021-02-24 12:42:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var reason = ["https://th.bing.com/th/id/OIP.v3s127JGzFAVljPnc0rceAAAAA?w=115&h=193&c=7&o=5&pid=1.7",
+ "https://th.bing.com/th/id/OIP.OxDomQkm4-hHMgWlpujB2wAAAA?w=182&h=283&c=7&o=5&pid=1.7", 
+ "https://th.bing.com/th/id/OIP.VSbkqmLBt1JNypdKp60eEQAAAA?w=146&h=180&c=7&o=5&pid=1.7", 
+ "https://th.bing.com/th/id/OIP.YIzWE-1SA7PbQdub3mEsVgHaGX?w=210&h=180&c=7&o=5&pid=1.7",
+  "https://th.bing.com/th/id/OIP.tX6euovTIkPExCrh3QDkSgHaFj?w=228&h=180&c=7&o=5&pid=1.7",
+   "https://th.bing.com/th/id/OIP.w_ujMZBEIPzWogbqSypfywAAAA?w=147&h=349&c=7&o=5&pid=1.7", 
+   "hhttps://th.bing.com/th/id/OIP.ZkRDO9v_wWTUn7ciBFDCCwHaNp?w=115&h=180&c=7&o=5&pid=1.7", 
+   "];
+var i = 0;
+function next() {
+    document.getElementById("image1").src = reason[i];
+    i++;
+}
